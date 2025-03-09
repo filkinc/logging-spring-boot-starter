39c1c586d7b974046a1b395ff487d2f867235746
filkinc <filkin.daniel@yandex.ru>
2025-03-09 16:48:50 +0300

    debug AutoConfiguration

--- src/main/java/ru/filkin/starter/loggingspringbootstarter/config/LoggingAutoConfiguration.java
+++ src/main/java/ru/filkin/starter/loggingspringbootstarter/config/LoggingAutoConfiguration.java
@@ -1,5 +1,6 @@
 package ru.filkin.starter.loggingspringbootstarter.config;
 
+
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -9,11 +10,17 @@ import ru.filkin.starter.loggingspringbootstarter.properties.LoggingProperties;
 
 @AutoConfiguration
 @EnableConfigurationProperties(LoggingProperties.class)
-@ConditionalOnProperty(prefix = "logging", name = "enabled", havingValue = "true", matchIfMissing = true)
+@ConditionalOnProperty(prefix = "logging.aspect", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class LoggingAutoConfiguration {
 
+    private final LoggingProperties loggingProperties;
+
+    public LoggingAutoConfiguration(LoggingProperties loggingProperties) {
+        this.loggingProperties = loggingProperties;
+    }
+
     @Bean
-    public LoggingAspect loggingAspect() {
-        return new LoggingAspect();
+    public LoggingAspect loggingAspect(LoggingProperties loggingProperties) {
+        return new LoggingAspect(loggingProperties);
     }
 }
